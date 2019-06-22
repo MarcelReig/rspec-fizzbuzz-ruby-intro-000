00c9466d70c5dfca40ccfb36686c1a6b6b58ec2a
Marcel Reig <marcelreig@gmail.com>
2019-06-22 14:38:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-      "Fizz"
-  end
+    "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  end
   elsif int % 5 == 0 && int % 3 == 0
     "FizzBuzz"
-  end
   else
     nil
   end
